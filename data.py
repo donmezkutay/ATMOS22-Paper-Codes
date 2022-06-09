45396808d282d2bf28f6cfd5f797ad1fd85627e7
Kutay Dönmez <60704340+donmezkutay@users.noreply.github.com>
2022-06-09 15:34:23 +0300

    #add codes to retrieve datasets analysis-ready

---
+++ data.py
@@ -0,0 +1,219 @@
+from glob import glob
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import rioxarray
+import xarray as xr
+
+from utils import *
+
+
+def clip_subroutine(dt, province, x_dims, y_dims):
+    """
+    subroutine to clip data to specific province
+    """
+    # projection and coordinate info
+    dt_proj = dt.rio.crs
+    
+    # open shapefile data
+    shapefile_path = r'data/shapefiles/Iller_HGK_6360_Kanun_Sonrasi.shp'
+    shapefile = gpd.read_file(shapefile_path)
+        
+    # define turkish to english encode-decode
+    turkish_encodes, turkish_decodes = create_encode_and_decode()
+    
+    # fix utf for the province column of the dataframe
+    shapefile['IL'] = shapefile['IL'].apply(
+        lambda x: fix_utf_problems(x, turkish_encodes, turkish_decodes)
+    ).str.lower()
+    
+    # check province name
+    if province not in shapefile['IL'].unique():
+        raise 'Inappropriate province name chosen'
+    
+    # query correspondent province from shapefile
+    province_shp = shapefile.query(f'IL == "{province}"')
+    
+    # clip data to correspondent province
+    clipped_dt = clip_to_city(dt, province_shp, dt_proj, x_dims, y_dims)
+    return clipped_dt
+    
+def retrieve_dmsp(province):
+    """
+    Adjusts and retrieves dmsp-ols light dataset
+        of corresponding province.
+    """
+    
+    # path related to province
+    il = 'common'
+    
+    # data source
+    data_source = 'dmsp'
+    
+    # nearest lon and lat information (to decrease data size)
+    lats = slice(42, 37)
+    lons = slice(25, 35)
+    
+    # define general path to datasets
+    general_path = f'data/{il}/{data_source}/*'
+
+    # get individual data links
+    data_links = glob(general_path)
+
+    dt_list = []
+    for link in data_links:
+        
+        # open data
+        dt = rioxarray.open_rasterio(link) \
+                      .squeeze() \
+                      .sel(y = lats, 
+                           x = lons)   
+
+        # assign date information
+        dt = define_dmsp_date(dt, link)
+        
+        # accumulate datasets
+        dt_list.append(dt)
+    
+    # merge datasets
+    merged_dt = xr.concat(dt_list, dim='time')    
+    
+    # assign data source and province attribute
+    merged_dt = merged_dt.assign_attrs({'data-source': data_source})
+    merged_dt = merged_dt.assign_attrs({'province':province})
+    
+    # coordinate names
+    x_dims = 'x'
+    y_dims = 'y'
+    
+    # short-cut clip to province
+    clipped_dt = clip_subroutine(merged_dt, province, x_dims, y_dims)
+    
+    return clipped_dt
+
+
+def retrieve_population(province):
+    """
+    Adjusts and retrieves population dataset
+        of corresponding province.
+    """
+    
+    # path related to province
+    il = 'common'
+    
+    # data source
+    data_source = 'population'
+    
+    # define general path to datasets
+    general_path = f'data/{il}/{data_source}/*'
+
+    # get individual data links
+    data_links = glob(general_path)
+
+    # open dataframe
+    dt = pd.read_excel(data_links[0])
+
+    # define turkish to english encode-decode
+    turkish_encodes, turkish_decodes = create_encode_and_decode()
+    
+    # fix encodes 
+    dt['Province'] = dt['Province'].apply(lambda x: fix_utf_problems(x, 
+                                                                     turkish_encodes,
+                                                                     turkish_decodes)) \
+                                                                     .str \
+                                                                     .lower()
+    # get province data
+    dt = dt.query(f'Province == "{province}"').reset_index(drop=True)
+    dt.attrs['data-source'] = data_source
+    dt.attrs['province'] = province
+    
+    return dt
+
+
+def retrieve_station(province):
+    """
+    Adjusts and retrieves station dataset
+        of corresponding province.
+    """
+    
+    data_source = 'station'
+    var_name = 'T' # possible: T
+    unit = 'degC'
+
+    # define general path to datasets
+    general_path = f'data/{province}/{data_source}/{var_name}.xlsx'
+
+    # get individual data links
+    data_links = glob(general_path)
+
+    # open dataframe
+    dt = pd.read_excel(data_links[0])
+    
+    # set attributes
+    dt.attrs['data-source'] = data_source
+    dt.attrs['var-name'] = var_name
+    dt.attrs['unit'] = unit
+    dt.attrs['province'] = province
+    
+    return dt
+
+
+def retrieve_corine(province):
+    """
+    Adjusts and retrieves corine dataset
+        of corresponding province.
+    """
+    
+    # path related to province
+    il = 'common'
+    
+    # data source
+    data_source = 'corine'
+    
+    # nearest lon and lat information (to decrease data size)
+    x = slice(48000,56000)
+    y = slice(31000,40000)
+    
+    # define general path to datasets
+    general_path = f'data/{il}/{data_source}/*'
+
+    # get individual data links
+    data_links = glob(general_path)
+    
+    dt_list = []
+    for link in data_links:
+        
+        # open data
+        dt = rioxarray.open_rasterio(link,
+                                     chunks='10mb') \
+                      .squeeze() \
+                      .isel(x=x, 
+                            y=y)
+        
+        # define date of the current data
+        dt = define_corine_date(dt, link)
+        
+        #accumulate datasets
+        dt_list.append(dt)
+        
+    # merge datasets
+    merged_dt = xr.concat(dt_list, dim='time')    
+    
+    # assign data source and province attribute
+    merged_dt = merged_dt.assign_attrs({'data-source': data_source})
+    merged_dt = merged_dt.assign_attrs({'province':province})
+    
+    # coordinate names
+    x_dims = 'x'
+    y_dims = 'y'
+    
+    # short-cut clip to province
+    clipped_dt = clip_subroutine(merged_dt, province, x_dims, y_dims)
+    
+    # turn nodata into np.nan
+    nodata = clipped_dt.rio.nodata
+    clipped_dt = clipped_dt.where(clipped_dt != nodata,
+                                  np.nan)
+    
+    return clipped_dt
---
+++ utils.py
@@ -0,0 +1,129 @@
+from datetime import datetime, timedelta
+from glob import glob
+
+import pyproj
+import rioxarray
+from shapely.geometry import mapping
+
+
+def define_modis_date(data, link):
+    
+    # define date of the data
+    index_date = link.find('A2') # date starts with A2 tag
+    year = link[index_date+1:][:4]
+    day = link[index_date+1:][4:7]
+    
+    # full date --> year + day
+    date = datetime(int(year), 1, 1) + timedelta(days = int(day)-1)
+    
+    # assign date as a coordinate to dataset
+    data = data.assign_coords({'time': date})
+    
+    return data
+
+def find_modis_proj(link):
+    
+    # crs of the data
+    return rioxarray.open_rasterio(link, engine='netcdf4').rio.crs
+
+
+def define_dmsp_date(data, link):
+    
+    # find F tag after which the date is located on the link
+    F_tag = link.find('F')
+    year = link[F_tag+3:F_tag+7]
+    
+    # assign time as a coord
+    data = data.assign_coords({'time': int(year)})
+    return data
+
+
+def define_corine_date(data, link):
+    
+    # find F tag after which the date is located on the link
+    F_tag = link.find('CLC')
+    year = link[F_tag+3:F_tag+7]
+    
+    # assign time as a coord
+    data = data.assign_coords({'time': int(year)})
+    return data
+
+
+def find_wrf_proj(data):
+    
+    proj = pyproj.Proj(proj='lcc', # projection type: Lambert Conformal Conic
+                   lat_1=data.attrs['TRUELAT1'],
+                   lat_2=data.attrs['TRUELAT2'], # Cone intersects with the sphere
+                   lat_0=data.attrs['MOAD_CEN_LAT'],
+                   lon_0=data.attrs['STAND_LON'], # Center point
+                   a=6370000,
+                   b=6370000).crs # The Earth is a perfect sphere
+    
+    return proj
+
+
+def create_encode_and_decode():
+    
+    t_alphabet = 'ÇçĞğİıÖöŞşÜüÂâ'
+    t_encodes = {}
+    t_decodes = {}
+    
+    for t in t_alphabet:
+        
+        match = {'Ç':'C',
+                 'ç':'c',
+                 'Ğ':'G',
+                 'ğ':'g',
+                 'İ':'I',
+                 'ı':'i',
+                 'Ö':'O',
+                 'ö':'o',
+                 'Ş':'S',
+                 'ş':'s',
+                 'Ü':'U',
+                 'ü':'u',
+                 'Â':'A',
+                 'â':'a',}
+        
+        # encode
+        t_encodes[t] = t.encode()
+        # decode
+        t_decodes[t_encodes[t]] = match[t]
+
+    return t_encodes, t_decodes
+
+
+def find_utf_problems(row):
+    
+    try:
+        if str(row.encode('UTF-8')).find('\\') != -1:
+            return 1
+        else:
+            return 0
+    except:
+        return 0
+    
+
+def fix_utf_problems(row, encodes, decodes):
+    
+    for enc,dec in zip(encodes.keys(), decodes.keys()):
+        
+        try:
+            if dec in row.encode():
+                row = row.replace(enc, decodes[dec])
+        except:
+            row = row
+            
+    return row
+
+
+def clip_to_city(data, shapefile, crs_data, x_dims, y_dims):
+    data= data.rio.set_spatial_dims(x_dim=x_dims, y_dim=y_dims)
+
+    data = data.rio.write_crs(crs_data)
+    
+    clipped = data.rio.clip(shapefile.geometry.apply(mapping),
+                            shapefile.crs, all_touched=True, 
+                            invert=False,)
+    
+    return clipped
